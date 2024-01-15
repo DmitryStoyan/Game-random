a86bfbbcbdfe9d991bd15c2bff6b924a7d860e3d
DmitryStoyan <jombee@yandex.ru>
2024-01-15 14:21:57 +0600

    added chance color

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 const startButton = document.querySelector(".start-button");
 
+// #808080
+// #9accff
+// #1800ff
+// #993367
+// #ff03ff
+// #ff0200
+// #ffcc01
+
 // const cells = 31;
 const cells = 81;
 
 const items = [
-  { name: "cs1.6", img: "images/game/cs1.6.png", chance: 1 },
-  { name: "left-4-dead-2", img: "images/game/left-4-dead-2.jpeg", chance: 2 },
-  { name: "day-of-defeat", img: "images/game/day-of-defeat.png", chance: 3 },
-  { name: "fortnite", img: "images/game/fortnite.jpeg", chance: 4 },
-  { name: "dayz", img: "images/game/dayz.png", chance: 5 },
-  { name: "gta5", img: "images/game/gta5.png", chance: 11 },
-  { name: "phasmophobia", img: "images/game/phasmophobia.png", chance: 13 },
-  { name: "pubg", img: "images/game/pubg.png", chance: 24 },
-  { name: "cs2", img: "images/game/cs2.png", chance: 29 },
+  { name: "cs1.6", img: "images/game/cs1.6.png", chance: 1, color: "#ffcc01" },
+  {
+    name: "left-4-dead-2",
+    img: "images/game/left-4-dead-2.jpeg",
+    chance: 2,
+    color: "#ff0200",
+  },
+  {
+    name: "day-of-defeat",
+    img: "images/game/day-of-defeat.png",
+    chance: 3,
+    color: "#ff03ff",
+  },
+  {
+    name: "fortnite",
+    img: "images/game/fortnite.jpeg",
+    chance: 4,
+    color: "#ff03ff",
+  },
+  { name: "dayz", img: "images/game/dayz.png", chance: 5, color: "#993367" },
+  { name: "gta5", img: "images/game/gta5.png", chance: 11, color: "#993367" },
+  {
+    name: "phasmophobia",
+    img: "images/game/phasmophobia.png",
+    chance: 13,
+    color: "#1800ff",
+  },
+  { name: "pubg", img: "images/game/pubg.png", chance: 24, color: "#9accff" },
+  { name: "cs2", img: "images/game/cs2.png", chance: 29, color: "#808080" },
 ];
 
 function getItem() {
@@ -42,12 +70,17 @@ function generateItems() {
     const item = getItem();
 
     const li = document.createElement("li");
+    const listChanceColor = document.createElement("div");
+    listChanceColor.className = "list__chance-color";
+    listChanceColor.style.backgroundColor = item.color;
+    listChanceColor.style.boxShadow = `0px -4px 10px ${item.color}`;
 
     li.setAttribute("data-item", JSON.stringify(item));
     li.classList.add("list__item");
     li.innerHTML = `
       <img class='list__item-image' src='${item.img}' alt=''>
     `;
+    li.appendChild(listChanceColor);
     list.append(li);
   }
 }
