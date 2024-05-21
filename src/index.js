590494ab731adfaa7a61f9e194177d5554403676
DmitryStoyan <jombee@yandex.ru>
2024-05-21 18:15:10 +0500

    fix bag2

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import { initialRouletteImg } from "./initialRouletteImg";
 export let isContentLoad = false;
 
 // export function closePreloader() {
+
 //   isContentLoad = true;
 //   preloader.remove();
 // }
